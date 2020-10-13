71074c95c86ee088013674550e15251b80520be3
Blake <bpro0821@gmail.com>
2020-10-12 20:05:35 -0500

    scaffolding and page setup

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {IonPage, IonHeader, IonToolbar, IonTitle} from "@ionic/react";
+
+
+const Home = () => {
+    return(
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>
+                        Hunt
+                    </IonTitle>
+                </IonToolbar>
+            </IonHeader>
+        </IonPage>
+    );
+};
+
+export default Home;
